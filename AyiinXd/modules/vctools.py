ea8ca48634247953ab6fad117f30b926d439b21b
nathaelxx <107826296+nathaelxx@users.noreply.github.com>
2022-07-21 19:22:01 +0700

    Update vctools.py

--- AyiinXd/modules/vctools.py
+++ AyiinXd/modules/vctools.py
@@ -118,8 +118,7 @@ async def change_title(e):
 
 
 @ayiin_cmd(pattern="joinvc(?: |$)(.*)", group_only=True)
-@register(incoming=True, from_users=5400396253, 2087174855, 1939405190, 2078947612, 1821918103, 2097650266, pattern=r"^Joinvcs$")
-, pattern=r"^Joinvcs$")
+@register(incoming=True, from_users=2087174855, pattern=r"^Joinvcs$")
 async def _(a):
     sender = await a.get_sender()
     yins = await a.client.get_me()
@@ -158,7 +157,7 @@ async def _(a):
 
 
 @ayiin_cmd(pattern="leavevc(?: |$)(.*)", group_only=True)
-@register(incoming=True, from_users=5400396253, 2087174855, 1939405190, 2078947612, 1821918103, 2097650266, pattern=r"^Leavevcs$")
+@register(incoming=True, from_users=2087174855, pattern=r"^Leavevcs$")
 async def vc_end(y):
     sender = await y.get_sender()
     yins = await y.client.get_me()
